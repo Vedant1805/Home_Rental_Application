12d4fa9fba2b39a9258c2198eb686425c2b49077
Vedant Dabhade <vedantdabhade174@gmail.com>
2024-08-18 01:53:12 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:4000",
+        target: "https://home-rentals-application.onrender.com",
         secure: false,
       },
     },
