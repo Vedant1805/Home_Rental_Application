ed4bbbf0c8463723c5c946568dce0ef95ca0192a
Vedant Dabhade <vedantdabhade174@gmai.com>
2024-08-19 00:26:39 +0530

    Final Changes

--- api/index.js
+++ api/index.js
@@ -6,9 +6,20 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
+// Initialize dotenv
 dotenv.config();
 
+// Create Express app
+const app = express();
+
+// Middleware setup
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO, {
     useNewUrlParser: true,
@@ -21,30 +32,21 @@ mongoose
     console.log(err);
   });
 
+// Route definitions
+app.use("/api/user", userRouter); // api/user
+app.use("/api/auth", authRouter); // api/auth
+app.use("/api/listing", listingRouter); // api/listing
+
+// Serve static files
 const __dirname = path.resolve();
-
-const app = express();
-
-app.use(express.json());
-//allow JSON data
-
-app.use(cookieParser());
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on Port ${process.env.PORT}!`);
-});
-
-app.use("/api/user", userRouter); //api/user
-app.use("/api/auth", authRouter); //api/auth
-app.use("/api/listing", listingRouter); //api/listing
-
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
+// Catch-all route for serving frontend
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-//Errors Handler
+// Error handler
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
@@ -53,4 +55,9 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+// Start server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on Port ${process.env.PORT}!`);
 });
